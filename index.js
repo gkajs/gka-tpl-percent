fa4b2a9b9122690e01f8ebc619ae0d84f5e24863
joeyguo <lgsincy@gmail.com>
2018-05-08 11:24:54 +0800

    v2.8.0

--- index.js
+++ index.js
@@ -2,32 +2,38 @@ var gkaUtils = require('gka-utils');
 var css = require("./lib/css"),
     html = require("./lib/html");
 
-var writeSync = gkaUtils.file.writeSync;
-
-module.exports = function (data, opts, cb) {
-    var dir = opts.imgOutput;
-
-    function run(data, opts, key) {
-        var name = (key? key + '-' : '') + 'gka',
-            cssName = name + '.css',
-            htmlName = name + '.html';
-
-        writeSync([dir, '..', cssName], css(data, opts));
-        writeSync([dir, '..', htmlName], html(data, opts, cssName));
-    }
-
-    run(data, opts);
-
-    // 对每个子目录都进行处理
-    gkaUtils._.effectSubFolderSync(run, data, opts);
-
-    cb && cb();
-};
-
-module.exports.config = function(opts) {
-    // console.log(opts)
-    return {
-        crop: false,
-        sprites: true,
-    }
-};
+module.exports = class PercentTemplatePlugin {
+    apply(compiler) {
+        compiler.hooks.on('templateOptions', (context, next) => {
+            context.options.crop = false;
+            context.options.sprites = true;
+            context.options.split = false;
+            context.options.diff = false;
+            next(context);
+        })
+        compiler.hooks.on('template', (context, next) => {
+            const {
+                options,
+                data,
+            } = context;
+            
+            const dir = options.imgOutput;
+                    
+            function run(data, opts, key) {
+                var name = (key? key + '-' : '') + 'gka',
+                    cssName = name + '.css',
+                    htmlName = name + '.html';
+                
+                context.assets[cssName] = css(data, opts);
+                context.assets[htmlName] = html(data, opts, cssName);
+            }
+        
+            run(data[0], options);
+        
+            // 对每个子目录都进行处理
+            gkaUtils._.effectSubFolderSync(run, data[0], options);
+        
+            next(context);
+        })
+    }        
+}
